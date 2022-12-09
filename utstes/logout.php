34f9e805dbec9e2980bffafd0a3db736dbab34d8
Prames Dyah Ayu <114793828+ayu47@users.noreply.github.com>
2022-12-09 19:41:31 +0700

    tugas pemograman3
    tugas pemograman3_ayu47

---
+++ utstes/logout.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include_once('koneksi.php');
+
+$username   	= $_SESSION['username'];
+$password   = $_SESSION['password'];
+$level      = $_SESSION['level']; 
+
+
+$query=mysqli_query($koneksi,"Update user set online=0 where username = '$username' and password = '$password' ");
+
+unset($_SESSION['username']);
+unset($_SESSION['password']);
+unset($_SESSION['level']); 
+
+header('Location: index.php');
+?>
